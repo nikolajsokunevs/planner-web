6026d4b9194eb60e80a6536cfaf2f42cf98049c8
nokunevs <n.okunevs@gmail.com>
2018-08-10 00:35:33 +0300

    OK

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
 $(function() {
   // page is now ready, initialize the calendar...
   $('#calendar').fullCalendar({
-    //dayClick:function (){
-    //  alert('it works works')
-    //}
+    dayClick:function (){
+      $('#dialog').dialog('open')
+    },
     theme: true
   })
+
+  $('#dialog').dialog({
+    autoOpen: false,
+    show:{
+      effect:'drop',
+      duration:500
+    },
+    hide:{
+      effect:'clip',
+      duration:300
+    }
+  });
+
+  $('.datepicker').datepicker();
+
+  $('#startTime').click(function(){
+
+  }
+
+
+  });
 });
+
+$(function() {
+  let $select = jQuery("#startTime");
+  for (let hr = 9; hr < 21; hr++) {
+  let hrStr = hr.toString().padStart(2, "0") + ":";
+  let val = hrStr + "00";
+  $select.append('<option val="' + val + '">' + val + '</option>');
+  val = hrStr + "30";
+  $select.append('<option val="' + val + '">' + val + '</option>')
+
+  });
