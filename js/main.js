6e47dc0c4e56789a443049d7ff4c65a268ee7012
Nikolay Okunev <nikolay.okunev@ctco.lv>
2018-08-10 13:24:56 +0300

    OK

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$(function() {
+$(function () {
   // page is now ready, initialize the calendar...
   $('#calendar').fullCalendar({
-    dayClick:function (){
+    dayClick: function () {
       $('#dialog').dialog('open')
     },
     theme: true
@@ -9,33 +9,44 @@ $(function() {
 
   $('#dialog').dialog({
     autoOpen: false,
-    show:{
-      effect:'drop',
-      duration:500
+    show: {
+      effect: 'drop',
+      duration: 500
     },
-    hide:{
-      effect:'clip',
-      duration:300
+    hide: {
+      effect: 'clip',
+      duration: 300
     }
   });
 
   $('.datepicker').datepicker();
 
-  $('#startTime').click(function(){
-
-  }
+  generateOptions(jQuery("#startTime"), 9.0, 15);
 
 
+  $('#endTime').click(function () {
+    var startTime=parseFloat($('#startTime option:selected').val());
+    generateOptions(jQuery("#endTime"), startTime+0.5, 15);
   });
+
 });
 
-$(function() {
-  let $select = jQuery("#startTime");
-  for (let hr = 9; hr < 21; hr++) {
-  let hrStr = hr.toString().padStart(2, "0") + ":";
-  let val = hrStr + "00";
-  $select.append('<option val="' + val + '">' + val + '</option>');
-  val = hrStr + "30";
-  $select.append('<option val="' + val + '">' + val + '</option>')
-
-  });
+function generateOptions (select, from, till) {
+  //let $select = jQuery("#startTime");
+  //for (let hr = from; hr <= till;) {
+    select.find('option').remove();
+    while(from<=till){
+    let hr = from
+    let fullHours=hr.toString().split('.')[0];
+    let minutes=hr.toString().split('.')[1];
+    var hrStr = fullHours.padStart(2, "0") + ":";
+    var hrStr="";
+    if (parseInt(minutes)==5){
+       hrStr = fullHours + ":30";
+    }else{
+      hrStr = fullHours + ":00";
+    }
+    select.append('<option value="' + hr + '">' + hrStr   + '</option>');
+    from=hr+0.5;
+  }
+}
